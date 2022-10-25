8763b9a6ba493f1fdef167f4cf6ad8f194a8e8b4
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-10-25 10:20:52 +0100

    SFML settings fixed and working

--- ZombieArena/main.cpp
+++ ZombieArena/main.cpp
@@ -6,9 +6,27 @@
 //
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Hello World");
+    sf::CircleShape shape(200.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
